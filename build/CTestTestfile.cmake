e63d284d0750fdd329a75e146bc6650f81e5e10e
Samuel Rud <18108728+rudx0029@users.noreply.github.com>
2022-04-20 20:10:52 -0600

    removed build dir from git

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/srud/projects/sandbox/dfs
-# Build directory: /home/srud/projects/sandbox/dfs/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
